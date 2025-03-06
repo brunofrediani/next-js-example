06e2c78717ed2bece61c1eb03ca36534e6d83bbd
Bruno Frediani <brunojfredi@gmail.com>
2025-03-06 01:03:29 -0300

    Funcao delete fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -135,7 +135,6 @@ WHERE id = ${id}
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
